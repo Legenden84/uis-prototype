0a7f973c98974c3c976445d043a8551ec3c5e990
Legenden84 <tobiaslegenden@gmail.com>
2023-03-13 23:47:52 +0100

    overflow

--- app.py
+++ app.py
@@ -13,7 +13,9 @@ external_stylesheets = [
 app = dash.Dash(__name__,
                 external_stylesheets=external_stylesheets,
                 suppress_callback_exceptions=True,
-                meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
+                meta_tags=[{'name': 'viewport',
+                            'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'}]
+)
 
 # server = app.server
 server = app.server
