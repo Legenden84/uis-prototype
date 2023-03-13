00e3e1b599744f432afac11a86ec2369d2e65126
Legenden84 <tobiaslegenden@gmail.com>
2023-03-13 20:33:44 +0100

    mobile

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ external_stylesheets = [
 ]
 
 # create dash object
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__,
+                external_stylesheets=external_stylesheets,
+                suppress_callback_exceptions=True,
+                meta_tags=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0'}])
 
 # server = app.server
 server = app.server
