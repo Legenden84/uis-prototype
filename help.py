e3a4312e72b7c3463af6ca90d698499c30dbb9cb
Legenden84 <tobiaslegenden@gmail.com>
2023-03-16 22:53:34 +0100

    fixed modal button, inside modal

--- help.py
+++ help.py
@@ -1,5 +1,57 @@
 import dash_bootstrap_components as dbc
 
+modal_open_sfo = dbc.Button("?",
+                                id="open-sfo",
+                                color="primary",
+                                style={
+                                "height": "20px",
+                                "width": "20px",
+                                "font-size": "14px",
+                                "font-family": "Ubuntu",
+                                "backgroundColor": "#16425D",
+                                "color": "white",
+                                "border-top-left-radius": "8px",
+                                "border-top-right-radius": "8px",
+                                "border-bottom-left-radius": "8px",
+                                "border-bottom-right-radius": "8px",
+                                "margin-right": "6px",
+                                "margin-top": "6px",
+                                })
+
+modal_sfo = dbc.Modal(
+    [
+        dbc.ModalHeader("Modal header"),
+        dbc.ModalBody("Modal body"),
+        dbc.ModalFooter(
+            dbc.Button("Luk",
+                        id="close-sfo",
+                        className="modal-button",
+                        style={
+                            "height": "35px",
+                            "width": "200px",
+                            "font-size": "20px",
+                            "font-family": "Ubuntu",
+                            "backgroundColor": "#16425D",
+                            "color": "white",
+                            "border-top-left-radius": "5px",
+                            "border-top-right-radius": "5px",
+                            "border-bottom-left-radius": "5px",
+                            "border-bottom-right-radius": "5px",
+                            })),
+    ],
+    id="modal-sfo",
+    style={
+        "position": "absolute",
+        "border": "2px solid black",
+        "margin-top": "200px",
+        "top": "25%",
+        "left": "50%",
+        "height": "525px",
+        "width": "350px",
+        "transform": "translate(-50%, -50%)",
+        "background-color": "#F7F7F7"}
+)
+
 modal_open_skole = dbc.Button("?",
                                 id="open-skole",
                                 color="primary",
@@ -23,7 +75,22 @@ modal_skole = dbc.Modal(
     [
         dbc.ModalHeader("Modal header"),
         dbc.ModalBody("Modal body"),
-        dbc.ModalFooter(dbc.Button("Close", id="close-skole", className="ml-auto")),
+        dbc.ModalFooter(
+            dbc.Button("Luk",
+                    id="close-skole",
+                    className="modal-button",
+                    style={
+                        "height": "35px",
+                        "width": "200px",
+                        "font-size": "20px",
+                        "font-family": "Ubuntu",
+                        "backgroundColor": "#16425D",
+                        "color": "white",
+                        "border-top-left-radius": "5px",
+                        "border-top-right-radius": "5px",
+                        "border-bottom-left-radius": "5px",
+                        "border-bottom-right-radius": "5px",
+                        })),
     ],
     id="modal-skole",
     style={
@@ -38,39 +105,3 @@ modal_skole = dbc.Modal(
         "background-color": "#F7F7F7"}
 )
 
-modal_open_sfo = dbc.Button("?",
-                                id="open-sfo",
-                                color="primary",
-                                style={
-                                "height": "20px",
-                                "width": "20px",
-                                "font-size": "14px",
-                                "font-family": "Ubuntu",
-                                "backgroundColor": "#16425D",
-                                "color": "white",
-                                "border-top-left-radius": "8px",
-                                "border-top-right-radius": "8px",
-                                "border-bottom-left-radius": "8px",
-                                "border-bottom-right-radius": "8px",
-                                "margin-right": "6px",
-                                "margin-top": "6px",
-                                })
-
-modal_sfo = dbc.Modal(
-    [
-        dbc.ModalHeader("Modal header"),
-        dbc.ModalBody("Modal body"),
-        dbc.ModalFooter(dbc.Button("Close", id="close-sfo", className="ml-auto")),
-    ],
-    id="modal-sfo",
-    style={
-        "position": "absolute",
-        "border": "2px solid black",
-        "margin-top": "200px",
-        "top": "25%",
-        "left": "50%",
-        "height": "525px",
-        "width": "350px",
-        "transform": "translate(-50%, -50%)",
-        "background-color": "#F7F7F7"}
-)
