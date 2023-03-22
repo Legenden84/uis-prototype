d2458e6513ad3a1967a9ade514eb9d1eb22559ae
Tobias Legenden <tobiaslegenden@gmail.com>
2023-03-22 17:25:17 +0100

    init: transparent modal 1-1

--- transparent_modal.py
+++ transparent_modal.py
@@ -1,2 +1,20 @@
 from dash import html
 import dash_bootstrap_components as dbc
+
+transparent_modal_1_1 = dbc.Modal(
+    [
+        dbc.ModalBody(
+            [
+                html.H1("Test")
+            ]
+        )
+    ],
+    id="transparent-modal-1-1",
+    keyboard=False,
+    backdrop="static",
+    style={
+        "border": "none",
+        "backgroundColor": "#D8E1E8",
+        "position": "absolute"
+    }
+)
